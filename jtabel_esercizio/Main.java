f50c833138d9b59a595726729f0b0317aff097b6
Nicolò <128604374+NicoMezzaa@users.noreply.github.com>
2023-04-03 16:21:00 +0200

    aggiunto esercizio con JTable finito
    Leggere un file csv contentente i campi codice_prodotto, nome_prodotto, descrizione_breve, prezzo_unitario e visualizzarlo in una JTable opportunamente costruita

---
+++ jtabel_esercizio/Main.java
@@ -0,0 +1,11 @@
+/*
+AUTORE: NICOLO' MEZZANZANICA
+DATA: 03/04/2023
+OGGETTO: CLASSE MAIN
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        new JTableGUI();
+    }
+}
